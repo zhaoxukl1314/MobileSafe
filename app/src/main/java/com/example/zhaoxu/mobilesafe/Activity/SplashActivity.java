3d9492dacf6bd7e6b79e420a0e3f39a52ad96c1e
Administrator <zhaoxukl1314@163.com>
2016-06-22 21:17:53 +0800

    Start project

---
+++ app/src/main/java/com/example/zhaoxu/mobilesafe/Activity/SplashActivity.java
@@ -0,0 +1,9 @@
+package com.example.zhaoxu.mobilesafe.Activity;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2016/6/22.
+ */
+public class SplashActivity extends Activity {
+}
